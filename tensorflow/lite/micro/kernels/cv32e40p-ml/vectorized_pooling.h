54884d3596da38ed6636222772e36b69d9b76a7a
hc3g17 <hc3g17@soton.ac.uk>
2020-12-03 16:57:41 +0000

    vectorized pooling operations

--- tensorflow/lite/micro/kernels/cv32e40p-ml/vectorized_pooling.h
+++ tensorflow/lite/micro/kernels/cv32e40p-ml/vectorized_pooling.h
@@ -9,7 +9,6 @@
 #include "tensorflow/lite/kernels/internal/types.h"
 
 #include "vector_operations.h"
-#include <iostream>
 
 namespace tflite {
 
@@ -31,7 +30,8 @@ inline void AveragePool(const PoolParams& params,
   const int stride_height = params.stride_height;
   const int stride_width = params.stride_width;
 
-std::cout<<"stride  "<<stride_height<<" "<<stride_width<<" "<<std::endl;
+  int32_t vecN;
+  const uint32_t vect_data_stride=depth;
 
   for (int batch = 0; batch < batches; ++batch) {
     for (int out_y = 0; out_y < output_height; ++out_y) {
@@ -45,18 +45,18 @@ std::cout<<"stride  "<<stride_height<<" "<<stride_width<<" "<<std::endl;
           const int filter_x_end = std::min(params.filter_width, input_width - in_x_origin);
           const int filter_y_start = std::max(0, -in_y_origin);
           const int filter_y_end = std::min(params.filter_height, input_height - in_y_origin);
-          std::cout<<"filter_y  "<<filter_y_start<<" "<<filter_y_end<<" "<<std::endl;
-          std::cout<<"filter_x  "<<filter_x_start<<" "<<filter_x_end<<" "<<std::endl;
+          vecN=filter_x_end-filter_x_start;
           int32_t acc = 0;
+          uint16_t tempAcc=0;
           int filter_count = 0;
           for (int filter_y = filter_y_start; filter_y < filter_y_end; ++filter_y) {
-            for (int filter_x = filter_x_start; filter_x < filter_x_end; ++filter_x) {
-              const int in_x = in_x_origin + filter_x;
-              const int in_y = in_y_origin + filter_y;
-              acc += input_data[Offset(input_shape, batch, in_y, in_x, channel)];
-              filter_count++;
-            }
+            const int in_x = in_x_origin; 
+            const int in_y = in_y_origin + filter_y;
+            const uint8_t* input_val = &input_data[Offset(input_shape, batch, in_y, in_x, channel)];
+            vectu_addReduction_stride(vecN, input_val, &tempAcc, tempAcc, vect_data_stride);
+            filter_count+=vecN;
           }
+          acc=static_cast<int32_t>(tempAcc);
           acc = (acc + filter_count / 2) / filter_count;
           acc = std::max(acc, params.quantized_activation_min);
           acc = std::min(acc, params.quantized_activation_max);
@@ -83,7 +83,8 @@ inline void AveragePool(const PoolParams& params,
   const int stride_height = params.stride_height;
   const int stride_width = params.stride_width;
 
-std::cout<<"stride  "<<stride_height<<" "<<stride_width<<" "<<std::endl;
+  int32_t vecN;
+  const uint32_t vect_data_stride=depth;
 
   for (int batch = 0; batch < batches; ++batch) {
     for (int out_y = 0; out_y < output_height; ++out_y) {
@@ -97,18 +98,18 @@ std::cout<<"stride  "<<stride_height<<" "<<stride_width<<" "<<std::endl;
           const int filter_x_end = std::min(params.filter_width, input_width - in_x_origin);
           const int filter_y_start = std::max(0, -in_y_origin);
           const int filter_y_end = std::min(params.filter_height, input_height - in_y_origin);
-          std::cout<<"filter_y  "<<filter_y_start<<" "<<filter_y_end<<" "<<std::endl;
-          std::cout<<"filter_x  "<<filter_x_start<<" "<<filter_x_end<<" "<<std::endl;
+          vecN=filter_x_end-filter_x_start;
           int32_t acc = 0;
+          int16_t tempAcc=0;
           int filter_count = 0;
           for (int filter_y = filter_y_start; filter_y < filter_y_end; ++filter_y) {
-            for (int filter_x = filter_x_start; filter_x < filter_x_end; ++filter_x) {
-              const int in_x = in_x_origin + filter_x;
-              const int in_y = in_y_origin + filter_y;
-              acc += input_data[Offset(input_shape, batch, in_y, in_x, channel)];
-              filter_count++;
-            }
+            const int in_x = in_x_origin; 
+            const int in_y = in_y_origin + filter_y;
+            const int8_t* input_val = &input_data[Offset(input_shape, batch, in_y, in_x, channel)];
+            vect_addReduction_stride(vecN, input_val, &tempAcc, tempAcc, vect_data_stride);
+            filter_count+=vecN;
           }
+          acc=static_cast<int32_t>(tempAcc);
           // Round to the closest integer value.
           acc = acc > 0 ? (acc + filter_count / 2) / filter_count : (acc - filter_count / 2) / filter_count;
           acc = std::max(acc, params.quantized_activation_min);
@@ -137,6 +138,10 @@ inline void MaxPool(const PoolParams& params, const RuntimeShape& input_shape,
   const int output_width = output_shape.Dims(2);
   const int stride_height = params.stride_height;
   const int stride_width = params.stride_width;
+
+  int32_t vecN;
+  const uint32_t vect_data_stride=depth;
+
   for (int batch = 0; batch < batches; ++batch) {
     for (int out_y = 0; out_y < output_height; ++out_y) {
       for (int out_x = 0; out_x < output_width; ++out_x) {
@@ -149,13 +154,13 @@ inline void MaxPool(const PoolParams& params, const RuntimeShape& input_shape,
           const int filter_x_end = std::min(params.filter_width, input_width - in_x_origin);
           const int filter_y_start = std::max(0, -in_y_origin);
           const int filter_y_end = std::min(params.filter_height, input_height - in_y_origin);
+          vecN=filter_x_end-filter_x_start;
           uint8_t max = 0;
           for (int filter_y = filter_y_start; filter_y < filter_y_end; ++filter_y) {
-            for (int filter_x = filter_x_start; filter_x < filter_x_end; ++filter_x) {
-              const int in_x = in_x_origin + filter_x;
-              const int in_y = in_y_origin + filter_y;
-              max = std::max(max, input_data[Offset(input_shape, batch, in_y, in_x, channel)]);
-            }
+            const int in_x = in_x_origin; 
+            const int in_y = in_y_origin + filter_y;
+            const uint8_t* input_val = &input_data[Offset(input_shape, batch, in_y, in_x, channel)];
+            vectu_maxReduction_stride(vecN, input_val, &max, max, vect_data_stride);
           }
           max = std::max<uint8_t>(max, params.quantized_activation_min);
           max = std::min<uint8_t>(max, params.quantized_activation_max);
@@ -184,6 +189,10 @@ inline void MaxPool(const PoolParams& params, const RuntimeShape& input_shape,
   const int output_width = output_shape.Dims(2);
   const int stride_height = params.stride_height;
   const int stride_width = params.stride_width;
+
+  int32_t vecN;
+  const uint32_t vect_data_stride=depth;
+
   for (int batch = 0; batch < batches; ++batch) {
     for (int out_y = 0; out_y < output_height; ++out_y) {
       for (int out_x = 0; out_x < output_width; ++out_x) {
@@ -196,13 +205,13 @@ inline void MaxPool(const PoolParams& params, const RuntimeShape& input_shape,
           const int filter_x_end = std::min(params.filter_width, input_width - in_x_origin);
           const int filter_y_start = std::max(0, -in_y_origin);
           const int filter_y_end = std::min(params.filter_height, input_height - in_y_origin);
+          vecN=filter_x_end-filter_x_start;
           int8_t max = std::numeric_limits<int8_t>::lowest();
           for (int filter_y = filter_y_start; filter_y < filter_y_end; ++filter_y) {
-            for (int filter_x = filter_x_start; filter_x < filter_x_end; ++filter_x) {
-              const int in_x = in_x_origin + filter_x;
-              const int in_y = in_y_origin + filter_y;
-              max = std::max(max, input_data[Offset(input_shape, batch, in_y, in_x, channel)]);
-            }
+            const int in_x = in_x_origin; 
+            const int in_y = in_y_origin + filter_y;
+            const int8_t* input_val = &input_data[Offset(input_shape, batch, in_y, in_x, channel)];
+            vect_maxReduction_stride(vecN, input_val, &max, max, vect_data_stride);
           }
           max = std::max<int8_t>(max, params.quantized_activation_min);
           max = std::min<int8_t>(max, params.quantized_activation_max);
